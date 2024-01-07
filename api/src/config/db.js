b052c65193df7134655f1982191ed420255a6c2e
Jesús Guijarro <72225659+JesusEduardoGuijarroSaldana@users.noreply.github.com>
2024-01-07 15:42:27 -0600

    :card_file_box: adding message for the db connection.

--- api/src/config/db.js
+++ api/src/config/db.js
@@ -1,10 +1,13 @@
 //Usuario - omarmontoya
 //Contraseña - Holamundo1
 const mongoose = require("mongoose");
-const urilocal = "mongodb://127.0.0.1:27017/tienda";
+const urilocal = "mongodb://mongo:27017/docker-node-mongo";
 const uriremota =
   "mongodb+srv://omarmontoya:Holamundo1@clusteromr.mlox5ph.mongodb.net/?retryWrites=true&w=majority";
 
-mongoose.connect(uriremota);
+mongoose
+  .connect(urilocal)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
 module.exports = mongoose;
